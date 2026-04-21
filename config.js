ed4656fe3602286201d37c37c122f40330496131
UbaidDecojent <93127485+UbaidDecojent@users.noreply.github.com>
2026-04-21 15:56:51 +0500

    url updateds

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.XOX_CONFIG = {
   // Leave this empty for Netlify static hosting.
   // Only set it to a real WebSocket backend, for example:
   // websocketUrl: "wss://your-node-server.example.com/ws"
-  websocketUrl: "https://xox-game-production.up.railway.app/",
+  websocketUrl: "wss://xox-game-production.up.railway.app/ws",
 };
